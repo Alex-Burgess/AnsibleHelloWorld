7a3018c23f97a3dfaf27db2365af104abe76d2d0
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2018-11-07 11:05:23 +0000

    Added monitoring.com website to the existing application load balancer.  Monitoring.com has it's own target group which directs traffic to the instances on port 8080.  There is also a listener rule on the ALB that looks for requests to /monitoring/* and forwards to this target group.

--- webfarmA/sites/monitoring.com/html/monitoring/index.php
+++ webfarmA/sites/monitoring.com/html/monitoring/index.php
@@ -1,5 +1,5 @@
 <?php
-  $ini = parse_ini_file('config/app.ini', true);
+  $ini = parse_ini_file('/var/www/html/monitoring.com/html/config/app.ini', true);
   $app_configuration = $ini['MAIN'];
 ?>
 <html>
@@ -10,7 +10,7 @@
    <h1>Monitoring (<?php echo $app_configuration['environmentName'] ?>)</h1>
 
    <font>Contents of this site:</font>
-   <a href="test_page1.html">Test page1</a>
-   <a href="test_page2.html">Test page2</a>
+   <a href="mon_page1.html">Test page1</a>
+   <a href="mon_page2.html">Test page2</a>
  </body>
 </html>
