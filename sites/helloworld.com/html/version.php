765732ee5fc66ecd7123326003d9163d22b50815
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2018-11-16 18:15:24 +0000

    Fixing two issues with adding branch or version to app configuration file.  Set a default value of master, if no branch is supplied.  Moved version config to a seperate file to avoid ansible wanting to overwrite the file and add it back.

--- sites/helloworld.com/html/version.php
+++ sites/helloworld.com/html/version.php
@@ -1,6 +1,9 @@
 <?php
   $ini = parse_ini_file('config/app.ini', true);
   $app_configuration = $ini['MAIN'];
+
+  $version_ini = parse_ini_file('config/version.ini', true);
+  $version_configuration = $version_ini['MAIN'];
 ?>
 <html>
  <head>
@@ -9,6 +12,6 @@
  <body>
    <h1>Version</h1>
    <h3>Environment: <?php echo $app_configuration['environmentName'] ?></h3>
-   <h3>Git branch/version: <?php echo $app_configuration['version'] ?></h3>
+   <h3>Git branch/version: <?php echo $version_configuration['version'] ?></h3>
  </body>
 </html>
